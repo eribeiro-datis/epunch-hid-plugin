f2be81aa761d76c8ceaf1a0c6d66278f0cddbb23
Eric Ribeiro <eric.james.ribeiro@gmail.com>
2017-05-03 11:06:55 -0400

    Try adding Context

--- src/android/com/datis/epunch/Hid.java
+++ src/android/com/datis/epunch/Hid.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
