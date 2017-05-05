c781f21766b31f2e784fcf0d580355fe3dd1604b
Eric Ribeiro <eric.james.ribeiro@gmail.com>
2017-05-05 14:14:18 -0400

    Add ref

--- src/android/com/datis/epunch/Hid.java
+++ src/android/com/datis/epunch/Hid.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.Provider;
 import java.util.List;
 
 import android.app.Activity;
