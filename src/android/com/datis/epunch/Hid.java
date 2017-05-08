923c3b0bf3fc13d9277fbad5900d488fec089afc
Eric Ribeiro <eric.james.ribeiro@gmail.com>
2017-05-08 15:59:38 -0400

    DSFID

--- src/android/com/datis/epunch/Hid.java
+++ src/android/com/datis/epunch/Hid.java
@@ -203,7 +203,7 @@ public class Hid extends CordovaPlugin {
                         byte[] bytes = new byte[] {
                             (byte)0xFF, // CLA
                             (byte)0x30, // INS
-                            (byte)0x02, // P1
+                            (byte)0x03, // P1
                             (byte)0x00, // P2
                             (byte)0x00 // Le
                         };
